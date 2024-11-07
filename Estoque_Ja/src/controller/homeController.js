de085399bdacbdff53de2d2f9f92f53da332ff3f
Octavio Leite Neto <otavio.gta@gmail.com>
2024-11-07 12:44:32 -0300

    clear

--- Estoque_Ja/src/controller/homeController.js
+++
@@ -1,9 +0,0 @@
-//const Usuario = require('../model/usuario');
-function homeView(req, res) {
-    res.render("home.html");
-}
-
-
-module.exports = {
-    homeView
-};
--- est_RB/2VerString.py
+++
@@ -1,9 +0,0 @@
-Word = input("Digite a palavra a qual deseja verificar o N de a's: ")
-cont = int(0)
-Word = Word.lower()
-
-for i in Word:
-    if 'a' in i:
-        cont +=1
-
-print("Foram encontrados {} a's".format(cont))
